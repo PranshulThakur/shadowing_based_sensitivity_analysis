d629f99f9048fca84f6daeddb504410e888ad699
PranshulThakur <pranshul777@gmail.com>
2024-07-25 14:29:26 -0400

    Updated plot file.

--- plot_from_file.py
+++ plot_from_file.py
@@ -1,12 +1,13 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+'''
 filename_sensitivity = "sensitivity_errors.txt";
 filename_time = "times.txt";
 
 sensitivity_errs = np.loadtxt(filename_sensitivity);
 T_array = np.loadtxt(filename_time);
-sensitivity_convergence_ref1 = 0.04/np.sqrt(T_array);
+sensitivity_convergence_ref1 = 0.025/np.sqrt(T_array);
 sensitivity_convergence_ref2 = 0.15/T_array;
 plt.loglog(T_array, sensitivity_errs,'*', label="Error in sensitivity");
 plt.loglog(T_array, sensitivity_convergence_ref1,'--', label="O(1/sqrt(T))");
@@ -16,4 +17,18 @@ plt.ylabel("Error in sensitivity",fontsize=12);
 plt.legend(fontsize=11);
 plt.savefig('newfig.eps', format='eps')
 plt.show();
-
+'''
+h_array = np.loadtxt('h_array.txt');
+sensitivity_errs_first_order = np.loadtxt('sensitivity_errs_first_order.txt');
+sensitivity_errs_second_order = np.loadtxt('sensitivity_errs_second_order.txt');
+expected_errs_second_order = 7.0*(h_array**2);
+expected_errs_first_order = 7.0*(h_array**1);
+plt.loglog(h_array, sensitivity_errs_first_order,'-*', label="First order discrete LSS");
+plt.loglog(h_array, expected_errs_first_order,'--', label=r"$\mathcal{O}(h)$");
+plt.loglog(h_array, sensitivity_errs_second_order,'-*', label="Second order discrete LSS");
+plt.loglog(h_array, expected_errs_second_order,'--', label=r"$\mathcal{O}(h^2)$");
+plt.xlabel("Grid size "+ r"$\Delta t$", fontsize=14);
+plt.ylabel("Error in sensitivity", fontsize=14);
+plt.legend(fontsize=11);
+plt.savefig('newfig.eps', format='eps')
+plt.show();
