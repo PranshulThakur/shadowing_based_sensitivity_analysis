1f317550b97c4dd0bc46e88a2537361d2c599044
PranshulThakur <pranshul777@gmail.com>
2025-04-15 17:26:36 -0400

    Updated other functions.

--- kuramoto_sivashinsky/main.py
+++ kuramoto_sivashinsky/main.py
@@ -101,16 +101,34 @@ def compute_adjoint_sensitivity(T, dt, s, delT, T_extra, T_net, n_int_grid_point
 def f_dot_adjoint_average_convergence_dt():
     n_runs = 10;
     n_grids = 5;
-    dt_array = np.zeros(n_grids);
-    f_dot_adjoint_average_array = np.zeros((n_grids,n_runs));
+    delT = 5.0;
+    T_net = 2500.0;
+    T_extra=50.0;
     T = 100.0;
     s = 0.0;
+    n_subspace_vectors = 20;
+    n_int_grid_points = 127; #127, 255, 511
+    dt_array = np.zeros(n_grids);
+    f_dot_adjoint_average_array = np.zeros((n_grids,n_runs));
     for i in range(n_grids):
         dt_array[i] = 0.1*(0.5**i);
     
-    for i in range(n_grids):
-        for j in range(n_runs):
-            f_dot_adjoint_average_array[i,j] = compute_adjoint_sensitivity(T,dt_array[i],s,True);
+    u0_array = np.zeros((n_runs,n_int_grid_points));
+    #for i in range(n_runs):
+        #u0_array[i,:] = np.random.uniform(-0.5,0.501,n_int_grid_points);
+    
+    #np.savetxt("u0_array.txt",u0_array);
+    u0_array = np.loadtxt("u0_array.txt");
+
+    for j in range(n_runs):
+        for i in range(n_grids):
+            m_total = round((T_net+T_extra)/dt_array[i]);
+            times_stored = np.zeros(m_total+1);
+            for k in range(m_total+1):
+                times_stored[k] = k*dt_array[i];
+            ks_solver = KuramotoSivashinsky(dt_array[i],m_total,n_int_grid_points,s);
+            u_stored = ks_solver.compute_trajectory(u0_array[j,:]);
+            f_dot_adjoint_average_array[i,j] = compute_adjoint_sensitivity(T, dt_array[i], s, delT, T_extra, T_net, n_int_grid_points, n_subspace_vectors, u_stored, times_stored, ks_solver,True); 
     '''    
     # plot convergence of f_dot_adjoint
     plt.figure();
@@ -129,20 +147,21 @@ def f_dot_adjoint_average_convergence_dt():
 def djbar_ds_vs_T():
     n_runs = 10; #10
     n_times = 20;
-    T_final = 500.0; #500.0
+    T_final = 2000.0; #500.0
     T_array = np.zeros(n_times);
     sensitivity_array = np.zeros( (n_times, n_runs));
-    dt = 0.05;
+    dt = 0.05/2;
     delT = 5.0;
-    T_net = 1000.0;
+    T_net = 2500.0;
     T_extra=50.0;
     n_subspace_vectors = 20;
     n_int_grid_points = 127; #127, 255, 511
     u0_array = np.zeros((n_runs,n_int_grid_points));
-    for i in range(n_runs):
-        u0_array[i,:] = np.random.uniform(-0.5,0.501,n_int_grid_points);
+    #for i in range(n_runs):
+        #u0_array[i,:] = np.random.uniform(-0.5,0.501,n_int_grid_points);
     
-    np.savetxt("u0_array.txt",u0_array);
+    #np.savetxt("u0_array.txt",u0_array);
+    u0_array = np.loadtxt("u0_array.txt");
     
     c_factor = pow(T_final/10.0,1.0/(n_times-1.0));
     for i in range(n_times):
@@ -177,18 +196,40 @@ def djbar_ds_vs_T():
 
 def djbar_ds_vs_s():
     n_runs = 10; #10
-    n_s = 10; #50
-    T = 500.0; 
+    n_s = 11; #50
+    T1 = 100.0; 
+    T2 = 1000.0; 
+    dt = 0.05/2;
+    delT = 5.0;
+    T_net = 2500.0;
+    T_extra=50.0;
+    n_subspace_vectors = 20;
+    n_int_grid_points = 127; #127, 255, 511
     s_array = np.zeros(n_s);
-    sensitivity_array = np.zeros( (n_s, n_runs));
-    dt = 0.05;
+    sensitivity_array_T1 = np.zeros( (n_s, n_runs));
+    sensitivity_array_T2 = np.zeros( (n_s, n_runs));
     
     for i in range(n_s):
         s_array[i] = -1.0 + i*2.0/(n_s-1.0);
+    
+    print("s_array = ",s_array); 
+    u0_array = np.zeros((n_runs,n_int_grid_points));
+    #for i in range(n_runs):
+        #u0_array[i,:] = np.random.uniform(-0.5,0.501,n_int_grid_points);
+    
+    #np.savetxt("u0_array.txt",u0_array);
+    u0_array = np.loadtxt("u0_array.txt");
+    m_total = round((T_net+T_extra)/dt);
+    times_stored = np.zeros(m_total+1);
+    for k in range(m_total+1):
+        times_stored[k] = k*dt;
 
-    for i in range(n_s):
-        for j in range(n_runs):
-            sensitivity_array[i,j] = compute_adjoint_sensitivity(T,dt,s_array[i]);
+    for j in range(n_runs):
+        for i in range(n_s):
+            ks_solver = KuramotoSivashinsky(dt,m_total,n_int_grid_points,s_array[i]);
+            u_stored = ks_solver.compute_trajectory(u0_array[j,:]);
+            sensitivity_array_T1[i,j] = compute_adjoint_sensitivity(T1, dt, s_array[i], delT, T_extra, T_net, n_int_grid_points, n_subspace_vectors, u_stored, times_stored, ks_solver); 
+            sensitivity_array_T2[i,j] = compute_adjoint_sensitivity(T2, dt, s_array[i], delT, T_extra, T_net, n_int_grid_points, n_subspace_vectors, u_stored, times_stored, ks_solver); 
     '''
     # plot sensitivity array
     plt.figure();
@@ -200,12 +241,11 @@ def djbar_ds_vs_s():
     plt.show();
     '''
     np.savetxt('s_array_djbar_ds_vs_s_runs.txt', s_array);
-    np.savetxt('sensitivity_array_djbar_ds_vs_s_runs.txt', sensitivity_array);
+    np.savetxt('sensitivity_arrayT1_djbar_ds_vs_s_runs.txt', sensitivity_array_T1);
+    np.savetxt('sensitivity_arrayT2_djbar_ds_vs_s_runs.txt', sensitivity_array_T2);
     return 0;
 
-#run_particular_initial_condition();
-#check_various_initial_conditions();
 #plot_primal_adjoint_solution_and_lyapunov_exponents();
-djbar_ds_vs_T();
-#djbar_ds_vs_s();
-#f_dot_adjoint_average_convergence_dt();
+#djbar_ds_vs_T();
+djbar_ds_vs_s();
+f_dot_adjoint_average_convergence_dt();
