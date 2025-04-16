f8bb2e368fbad5c7e95522037625757f2f9b6e22
PranshulThakur <pranshul777@gmail.com>
2025-04-16 16:04:20 -0400

    Updated plotting functions.

--- kuramoto_sivashinsky/plot_figures.py
+++ kuramoto_sivashinsky/plot_figures.py
@@ -58,7 +58,7 @@ fig.colorbar(contourplot,pad=0.15,label=r'$\psi$');
 plt.savefig('adjoint_ks.png', format='png');
 plt.show();
 '''
-
+'''
 # Sensitivity - T plot
 T_array = np.loadtxt("T_array_djbar_ds_vs_T_runs.txt");
 sensitivity_array = np.loadtxt("sensitivity_array_djbar_ds_vs_T_runs.txt");
@@ -98,50 +98,49 @@ plt.ylabel(r"$d\bar{J}/ds$",fontsize=12);
 #plt.ylim([-1.5,0.0]);
 plt.savefig("djds_vs_T_ks.pdf",format="pdf");
 plt.show();
-
+'''
 '''
 # Sensitivity - s plot
 # plot sensitivity array
-s_array_50 = np.loadtxt("s_array_djbar_ds_vs_s_runs50.txt");
-sensitivity_array_50 = np.loadtxt("sensitivity_array_djbar_ds_vs_s_runs50.txt");
-s_array_500 = np.loadtxt("s_array_djbar_ds_vs_s_runs255.txt");
-sensitivity_array_500 = np.loadtxt("sensitivity_array_djbar_ds_vs_s_runs255.txt");
+s_array = np.loadtxt("s_array_djbar_ds_vs_s_runs.txt");
+sensitivity_array_T1 = np.loadtxt("sensitivity_arrayT1_djbar_ds_vs_s_runs.txt");
+sensitivity_array_T2 = np.loadtxt("sensitivity_arrayT2_djbar_ds_vs_s_runs.txt");
 plt.figure();
 n_runs=10;
-n_s = len(s_array_500);
-average_50 = mean_array(sensitivity_array_50, s_array_50, n_s, n_runs);
-std_dev_50 = std_dev_array(sensitivity_array_50, s_array_50, n_s, n_runs,average_50);
-sensitivity_50_lower = average_50-std_dev_50;
-sensitivity_50_upper = average_50+std_dev_50;
-average_500 = mean_array(sensitivity_array_500, s_array_500, n_s, n_runs);
-std_dev_500 = std_dev_array(sensitivity_array_500, s_array_500, n_s, n_runs,average_500);
-sensitivity_500_lower = average_500-std_dev_500;
-sensitivity_500_upper = average_500+std_dev_500;
-#for j in range(n_runs):
-#    if j==0:
-#        plt.plot(s_array_50, sensitivity_array_50[:,j],'*',color='red', label="T=50");
-#        plt.plot(s_array_500, sensitivity_array_500[:,j],'*',color='blue',label="T=500");
-#    else:
-#        plt.plot(s_array_50, sensitivity_array_50[:,j],'*',color='red');
-#        plt.plot(s_array_500, sensitivity_array_500[:,j],'*',color='blue');
-plt.plot(s_array_50, average_50, '*',color="red", label="T=50");
-plt.fill_between(s_array_50, sensitivity_50_lower, sensitivity_50_upper, alpha=0.3, color="red");
-plt.plot(s_array_500, average_500, '*',color="blue", label="T=500");
-plt.fill_between(s_array_500, sensitivity_500_lower, sensitivity_500_upper, alpha=0.3, color="blue");
+n_s = 11;
+average_T1 = mean_array(sensitivity_array_T1, s_array, n_s, n_runs);
+std_dev_T1 = std_dev_array(sensitivity_array_T1, s_array, n_s, n_runs,average_T1);
+sensitivity_T1_lower = average_T1-std_dev_T1;
+sensitivity_T1_upper = average_T1+std_dev_T1;
+average_T2 = mean_array(sensitivity_array_T2, s_array, n_s, n_runs);
+std_dev_T2 = std_dev_array(sensitivity_array_T2, s_array, n_s, n_runs,average_T2);
+sensitivity_T2_lower = average_T2-std_dev_T2;
+sensitivity_T2_upper = average_T2+std_dev_T2;
+for j in range(n_runs):
+    if j==0:
+        plt.plot(s_array, sensitivity_array_T1[:,j],'*',color='red', label="T=100");
+        plt.plot(s_array, sensitivity_array_T2[:,j],'*',color='blue',label="T=1000");
+    else:
+        plt.plot(s_array, sensitivity_array_T1[:,j],'*',color='red');
+        plt.plot(s_array, sensitivity_array_T2[:,j],'*',color='blue');
+plt.plot(s_array, average_T1, '*',color="red", label="T=100");
+plt.fill_between(s_array, sensitivity_T1_lower, sensitivity_T1_upper, alpha=0.3, color="red");
+plt.plot(s_array, average_T2, '*',color="blue", label="T=1000");
+plt.fill_between(s_array, sensitivity_T2_lower, sensitivity_T2_upper, alpha=0.3, color="blue");
 plt.xlabel('s',fontsize=12);
 plt.ylabel(r"$d\bar{J}/ds$",fontsize=12);
-plt.ylim([-2.0,0.0]);
+#plt.ylim([-2.0,0.0]);
 plt.legend();
 plt.savefig("djds_vs_s_ks.pdf",format="pdf");
 plt.show();
 '''
-'''
+
 # plot convergence of f_dot_adjoint
 dt_array = np.loadtxt("dt_array_f_dot_adjoint.txt");
 f_dot_adjoint_average_array = np.loadtxt("f_dot_adjoint_runs_array.txt");
 n_runs=10;
 C=10;
-expected_errors = C*(dt_array**3);
+expected_errors = C*(dt_array**4);
 plt.figure();
 for j in range(n_runs):
     if j==0:
@@ -149,11 +148,11 @@ for j in range(n_runs):
     else:
         plt.loglog(dt_array, f_dot_adjoint_average_array[:,j],'*',color='blue');
 
-plt.loglog(dt_array,expected_errors,'--',label=r"$\mathcal{O}(\Delta t^3)$",color="red");
+plt.loglog(dt_array,expected_errors,'--',label=r"$\mathcal{O}(\Delta t^4)$",color="red");
 
 plt.xlabel(r"$\Delta t$",fontsize=12);
 plt.ylabel(r"$\frac{1}{T}\int_0^T\psi^Tfdt$",fontsize=12);
 plt.legend(fontsize=12);
 plt.savefig("adjoint_neutral_convergence_ks.eps",format="eps");
 plt.show();
-'''
+
